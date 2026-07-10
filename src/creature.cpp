b3517f550956fc3d6a52e767cc7b289783f336fe
Jediluke1999 <jediluke1999@gmail.com>
2026-07-10 02:21:14 -0500

    Declared.

--- src/creature.cpp
+++ src/creature.cpp
@@ -1,21 +1,3 @@
-#include <iostream> 
-
-enum status {
-    STARVE,
-    POISON,
-    POKE
-};
-
 
 class creature {
-    private:
-        int health = 100;
-        int hunger = 100;
-
-    public:
-        int status_values[] {0,0,0};
-
-        void tick() {
-        
-        };
 };
