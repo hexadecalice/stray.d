1aab6225905c804cde3b976b0a54ea2e9105e456
Jediluke1999 <jediluke1999@gmail.com>
2026-07-10 02:45:05 -0500

    Adds the status effects values-array and the creature's enumerable afflictions.

--- src/creature.cpp
+++ src/creature.cpp
@@ -1,9 +1,16 @@
 #define MAX_HEALTH 100
 #define MAX_HUNGER 100
 
+enum status {
+    STARVE,
+    POISON,
+    POKE,
+    MAX_STATUS // Not a real status. Helps us count how many we've implemented.
+};
 
 class creature {
     private:
         int health = MAX_HEALTH;
         int hunger = MAX_HUNGER;
+        int status_values[MAX_STATUS]{0};
 };
