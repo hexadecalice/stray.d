c3e472449bc3dfb283d66e6a03b4ac71e4f9584a
hexadecalice <hexadecalice@wonderland.attlocal.net>
2026-07-09 21:30:30 -0700

    Added barebones for accepting network connectivity on a given port

--- src/network.cpp
+++ src/network.cpp
@@ -1,7 +1,60 @@
 #include <iostream> 
+#include <SFML/Network.hpp>
+#include <string>
 
+void setup_listener(sf::TcpListener &listener, int port) { 
+
+	if(listener.listen(port) != sf::Socket::Done) { 
+
+		std::cout << "Sorry! Couldn't bind to port.";
+		exit(-1);
+	}
+
+	listener.setBlocking(false); 
+
+	std::cout << "Success! Bound to port " << port << "."; 
+
+
+}
+
+void listen_to_port(sf::TcpListener &listener) { 
+
+	sf::TcpSocket client; 
+
+	char buffer[1024];
+	std::size_t received; 
+
+
+	if(listener.accept(client) == sf::Socket::Done) { 
+
+		if(client.receive(buffer, sizeof(buffer), received) == sf::Socket::Done) { 
+				std::string command(buffer, received); 
+				std::cout << command;
+
+		}
+
+	}
+	else { 
+
+
+	}
+
+
+
+
+}
 
 int main(void) { 
+
+
+	sf::TcpListener my_listener; 
+	setup_listener(my_listener, 6767);
+
+	while(true) { 
+		listen_to_port(my_listener);
+		sf::sleep(sf::milliseconds(100));
+
+	}
 
 
 	return 0; 
