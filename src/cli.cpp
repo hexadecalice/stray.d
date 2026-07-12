f0b3d09fda81f392d2af2b410821715f1b9f4c47
Jediluke1999 <jediluke1999@gmail.com>
2026-07-11 21:13:54 -0500

    Problem documentation in a multi-level comment.

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,5 +1,22 @@
 #include <iostream> 
 
+/*
+ * So here's the deal with console graphics/scaling:
+ * Apple/Linux uses <sys/ioctl>
+ * Windows uses <windows.h>
+ *
+ * Two different header files, but we can use #ifdef logic
+ * to get the terminal variables we need on each platform.
+ * Fortunately, we only need three settings:
+ * >> Height
+ * >> Width
+ * >> ANSI-Support
+ *
+ * ...but we're also neglecting terminal events such as resizing,
+ * meaning we need to do a lot of pre-processing or completely split
+ * some elements of the current cli class.
+*/
+
 int main(void) { 
 return 0;
 }
